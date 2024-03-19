3bf37c142f71511b4ecad78710806fea22d1ef17
Zita <zitaagada@gmail.com>
2024-03-19 19:57:20 +0100

    Zita: These are my works done on the master branch

--- app/app.js
+++ app/app.js
@@ -32,6 +32,16 @@ app.get("/resident/generate-code", function (req, res) {
   res.render("residentPages/codeGenerator");
 });
 
+// Route for Checking History logs
+app.get("/resident/accesslogs", function (req, res) {
+  res.render("residentPages/access-logs");
+});
+
+// Route for Resident profile
+app.get("/resident/residentprofile", function (req, res) {
+  res.render("residentPages/resident_profile");
+});
+
 // ---- SECURITY ROUTES ----
 
 // Route for registering residents
@@ -46,6 +56,10 @@ app.get("/security/verify-code", function (req, res) {
 
 app.get("/security/profile", function (req, res) {
   res.render("securityPages/profile-security");
+});
+
+app.get("/security/resident-list", function (req, res) {
+  res.render("securityPages/resident_list");
 });
 
 // ACCESS LOGS PAGE (SECURITY)----

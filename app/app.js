72189cf0ba562a3a074db7481285b10efef32f0d
chinaza-Sommie <chinazachukwunweike35@gmail.com>
2024-03-19 12:25:58 +0000

    added pug files and server setup

--- app/app.js
+++ app/app.js
@@ -4,47 +4,83 @@ const express = require("express");
 // Create express app
 var app = express();
 
+// views connection
+app.set("view engine", "pug");
+app.set("views", "./app/views");
+
 // Add static files location
 app.use(express.static("static"));
 
 // Get the functions in the db.js file to use
-const db = require('./services/db');
-
-//
+const db = require("./services/db");
 
 // Create a route for root - /
-app.get("/", function(req, res) {
-    res.send("Hello world!");
+app.get("/", function (req, res) {
+  // res.render("resident-layout");
+  res.render("codeGenerator");
+});
+
+// Route for login page
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+// ------ RESIDENT ROUTES ----
+
+// Route for Generating code
+app.get("/resident/generate-code", function (req, res) {
+  res.render("resident-layout/codeGenerator");
+});
+
+// ---- SECURITY ROUTES ----
+
+// Route for registering residents
+app.get("/security/register-resident", function (req, res) {
+  res.render("securityPages/registerResident");
+});
+
+// Route for verifying residents codes
+app.get("/security/verify-code", function (req, res) {
+  res.render("securityPages/verifycode");
+});
+
+app.get("/security/profile", function (req, res) {
+  res.render("securityPages/profile-security");
+});
+
+// ACCESS LOGS PAGE (SECURITY)----
+app.get("/security/access-logs", function (req, res) {
+  res.render("securityPages/access-logs");
 });
 
 // Create a route for testing the db
-app.get("/db_test", function(req, res) {
-    // Assumes a table called test_table exists in your database
-    sql = 'select * from test_table';
-    db.query(sql).then(results => {
-        console.log(results);
-        res.send(results)
-    });
+app.get("/db_test", function (req, res) {
+  // Assumes a table called test_table exists in your database
+  sql = "select * from test_table";
+  db.query(sql).then((results) => {
+    console.log(results);
+    res.send(results);
+  });
 });
 
 // Create a route for /goodbye
 // Responds to a 'GET' request
-app.get("/goodbye", function(req, res) {
-    res.send("Goodbye world!");
+app.get("/goodbye", function (req, res) {
+  res.send("Goodbye world!");
 });
 
 // Create a dynamic route for /hello/<name>, where name is any value provided by user
 // At the end of the URL
 // Responds to a 'GET' request
-app.get("/hello/:name", function(req, res) {
-    // req.params contains any parameters in the request
-    // We can examine it in the console for debugging purposes
-    console.log(req.params);
-    //  Retrieve the 'name' parameter and use it in a dynamically generated page
-    res.send("Hello " + req.params.name);
+app.get("/hello/:name", function (req, res) {
+  // req.params contains any parameters in the request
+  // We can examine it in the console for debugging purposes
+  console.log(req.params);
+  //  Retrieve the 'name' parameter and use it in a dynamically generated page
+  res.send("Hello " + req.params.name);
 });
 
 // Start server on port 3000
-app.listen(3000,function(){
-    console.log(`Server running at http://127.0.0.1:3000/`);
+app.listen(3000, function () {
+  console.log(`Server running at http://127.0.0.1:3000/`);
 });

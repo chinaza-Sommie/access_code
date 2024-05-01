42245c9556be377b14e72122b739138b2574f0c7
chinaza-Sommie <chinazachukwunweike35@gmail.com>
2024-05-01 10:41:23 +0100

    app.js modifications

--- app/app.js
+++ app/app.js
@@ -1,6 +1,5 @@
 // Import express.js
 const express = require("express");
-const { User } = require("./models/user");
 
 const { User } = require("./models/users");
 const { Alerts } = require("./models/alerts");
@@ -9,23 +8,19 @@ const { Codes } = require("./models/codes");
 // Create express app
 var app = express();
 
-// Set the sesssions
-var session = require('express-session');
-app.use(session({
-  secret: 'secretkeysdfjsflyoifasd',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
+// accept form input
+app.use(express.urlencoded({ extended: true }));
 
 // Sessions to login
-var session = require('express-session');
-app.use(session({
-  secret: 'secretkeysdfjsflyoifasd',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
+var session = require("express-session");
+app.use(
+  session({
+    secret: "secretkeysdfjsflyoifasd",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 
 // views connection
 app.set("view engine", "pug");
@@ -36,11 +31,9 @@ app.use(express.static("static"));
 
 // Get the functions in the db.js file to use
 const db = require("./services/db");
-app.use(express.urlencoded({ extended: true}))
-
 
 // Create a route for root - /
-app.get("/", function (req, res) { 
+app.get("/", function (req, res) {
   res.render("landingpage");
 });
 
@@ -49,27 +42,15 @@ app.get("/code_generator", function (req, res) {
   res.render("residentPages/codeGenerator");
 });
 
-// Create a route for root - /
-app.get("/", function(req, res) {
-  console.log(req.session);
-  if (req.session.uid) {
-  res.send('Welcome back, ' + req.session.uid + '!');
-} else {
-  res.send('Please login to view this page!');
-}
-res.end();
-});
-
 // Route for login page
 app.get("/login", function (req, res) {
-  
   console.log(req.session);
   if (req.session.uid) {
-		res.redirect('/resident/generate-code');
-	} else {
-		res.render("login");
+    res.redirect("/resident/generate-code");
+  } else {
+    res.render("login");
     res.end();
-	}
+  }
 });
 
 // Route for landing page
@@ -82,44 +63,40 @@ app.get("/landing_page", function (req, res) {
 // Route for Generating code
 app.get("/resident/generate-code", function (req, res) {
   res.render("residentPages/codeGenerator");
-  
+
   // console.log(req.session);
   // if (req.session.uid) {
-	// 	res.render("residentPages/codeGenerator");
-	// } else {
-	// 	res.render("login", {errorMessage: 'Please Login to view that page' });
+  // 	res.render("residentPages/codeGenerator");
+  // } else {
+  // 	res.render("login", {errorMessage: 'Please Login to view that page' });
   //   res.end();
   // }
 });
 
 // Route for Checking History logs
 app.get("/resident/accesslogs/:id", function (req, res) {
-    var resLog = req.params.id;
-    var accesssql = 
+  var resLog = req.params.id;
+  var accesssql =
     "SELECT ct.Code_Value as code, ct.Visitors_Name as visitors, ct.Code_Status as status, ut.User_Name as name from codes_table ct JOIN user_table ut on ut.User_ID = ct.User_ID WHERE ct.User_ID = ?";
 
-    db.query(accesssql, [resLog]).then((results) => {
-        res.render("residentPages/access-logs", { data: results });
-        
-    });
-  
+  db.query(accesssql, [resLog]).then((results) => {
+    res.render("residentPages/access-logs", { data: results });
+  });
 });
 
 // Route for Resident profile
 app.get("/profile/:id", function (req, res) {
-    var resProfile = req.params.id;
+  var resProfile = req.params.id;
 
-    var residentSql = "SELECT * FROM user_table WHERE User_ID = ?";
+  var residentSql = "SELECT * FROM user_table WHERE User_ID = ?";
 
-    db.query(residentSql, [resProfile]).then((results) => {
-        
-        if (results[0].User_role == 'Resident'){
-            res.render("residentPages/resident-profile", { data: results[0] });
-        }else{
-            res.render("securityPages/profile-security", { data: results[0] });
-        }
-     
-    });
+  db.query(residentSql, [resProfile]).then((results) => {
+    if (results[0].User_role == "Resident") {
+      res.render("residentPages/resident-profile", { data: results[0] });
+    } else {
+      res.render("securityPages/profile-security", { data: results[0] });
+    }
+  });
 });
 
 // ---- SECURITY ROUTES ----
@@ -134,147 +111,121 @@ app.get("/security/verify-code", function (req, res) {
   res.render("securityPages/verifycode");
 });
 
-
 // Route for send alert
 app.get("/security/alert", function (req, res) {
   res.render("securityPages/alert");
 });
 
-
 app.get("/security/resident-list", function (req, res) {
-    var userTableSql = 
-    "SELECT * FROM user_table WHERE User_role = 'Resident'";
-    db.query(userTableSql).then((results) => {
-        res.render("securityPages/resident_list", { data: results });
-
-    });
-    // res.render("securityPages/resident_list");
+  var userTableSql = "SELECT * FROM user_table WHERE User_role = 'Resident'";
+  db.query(userTableSql).then((results) => {
+    res.render("securityPages/resident_list", { data: results });
+  });
+  // res.render("securityPages/resident_list");
 });
 
-
-
 // VISITORS LOG PAGE (SECURITY)
-app.get("/security/visitors-log", function (req, res){
-  var sql = 
+app.get("/security/visitors-log", function (req, res) {
+  var sql =
     "SELECT ct.Code_Value as code, ct.Visitors_Name as visitors, ct.Code_Status as status, ut.User_Name as name from codes_table ct JOIN user_table ut on ut.User_ID = ct.User_ID";
 
   db.query(sql).then((results) => {
-      res.render("securityPages/visitors-log", { data: results });
-      // console.log(results)
+    res.render("securityPages/visitors-log", { data: results });
+    // console.log(results)
   });
 });
 
-// Logout
-app.get('/logout', function (req, res) {
-  req.session.destroy();
-  res.redirect('/login');
-});
-
-app.post('/add-register', async function (req, res) {
+app.post("/send-alert", async function (req, res) {
   params = req.body;
-  var user = new User(params.email);
-  try {
-      uId = await user.getIdFromEmail();
-      if (uId) {
-          // If a valid, existing user is found, set the password and redirect to the users single-student page
-          await user.setUserPassword(params.password);
-          console.log(req.session.id);
-          res.send('Password set successfully');
-      }
-      else {
-          // If no existing user is found, add a new one
-          newId = await user.addUser(params.email);
-          res.send('Perhaps a page where a new user sets a programme would be good here');
-      }
-  } catch (err) {
-      console.error(`Error while adding password `, err.message);
+  if (params.message == "") {
+    res.render("securityPages/alert", {
+      errorMessage: "Oops!! This field cannot be empty. Try again.",
+    });
+  } else {
+    try {
+      params = req.body;
+
+      senderId = 1;
+      alerts = new Alerts(senderId);
+      await alerts.postAlerts(params.message, senderId);
+      console.log(alerts);
+      res.render("securityPages/alert", {
+        successMessage: "Alert message has been sent successfuly!",
+      });
+    } catch (err) {
+      res.render("securityPages/alert", { errorMessage: err.message });
+    }
   }
 });
-
-// Check submitted email and password pair
-app.post('/authenticate', async function (req, res) {
-  params = req.body;
-  var user = new User(params.email);
-  try {
-      uId = await user.getIdFromEmail();
-      if (uId) {
-          match = await user.authenticate(params.password);
-          if (match) {
-              req.session.uid = uId;
-              req.session.loggedIn = true;
-              console.log(req.session.id);
-              res.redirect('/student-single/' + uId);
-          }
-          else {
-              // TODO improve the user journey here
-              res.send('invalid password');
-          }
-      }
-      else {
-          res.send('invalid email');
-      }
-  } catch (err) {
-      console.error(`Error while comparing `, err.message);
-  }
-});
-
-
 
 app.post("/login-auth", async function (req, res) {
   params = req.body;
-    var user = new User(params.email);
-    try {
-        uId = await user.getIdFromEmail();
-        
-        if (uId) {
-            match = await user.authenticate(params.password);
+  var user = new User(params.email);
+  try {
+    uId = await user.getIdFromEmail();
 
-            if (match) {
-                req.session.uid = uId;
-                req.session.loggedIn = true;
-                console.log(req.session.id);
-                res.redirect("/resident/generate-code");
-            }
-            else {
-                
-                res.render("login", {errorMessage: 'Oops!! Invalid Email/Password. Try again.' });
-            }
-        }
-        else {
-          res.render("login", {errorMessage: 'Oops!! Invalid Email/Password. Try again.' });
-        }
-    } catch (err) {
-        console.error(`Error while comparing `, err.message);
+    if (uId) {
+      match = await user.authenticate(params.password);
+
+      if (match) {
+        req.session.uid = uId;
+        req.session.loggedIn = true;
+        console.log(req.session.id);
+        res.redirect("/resident/generate-code");
+      } else {
+        res.render("login", {
+          errorMessage: "Oops!! Invalid Email/Password. Try again.",
+        });
+      }
+    } else {
+      res.render("login", {
+        errorMessage: "Oops!! Invalid Email/Password. Try again.",
+      });
     }
+  } catch (err) {
+    console.error(`Error while comparing `, err.message);
+  }
 });
 
 app.post("/access-code-generator", async function (req, res) {
   params = req.body;
-  var visitorsName = params.name
-  var purpose = params.purpose
-  var location = params.location
-  var timeExpired = params.timeExpired
-  var codeStatus = "Used"
-  var codeValue = 5000
+  var visitorsName = params.name;
+  var purpose = params.purpose;
+  var location = params.location;
+  var timeExpired = params.timeExpired;
+  var codeStatus = "Used";
+  var codeValue = 5000;
   // Assuming you have session information to retrieve user ID
   const userId = req.session.uid;
-  
+
   try {
-      // Create a new instance of Code
-      const codes = new Codes(userId);
-      console.log(timeExpired)
-      // Add the code to the database
-      const result = await codes.addCode(codeValue, visitorsName, codeStatus, timeExpired);
-      // var codex = await codes.generateCode()
-      // console.log(codex)
-      if (result) {
-          res.render("residentPages/codeGenerator", { successMessage: 'Code generated successfully.' });
-      } else {
-          res.render("residentPages/codeGenerator", { errorMessage: 'Failed to generate code. Please try again.' });
-      }
+    // Create a new instance of Code
+    const codes = new Codes(userId);
+    console.log(timeExpired);
+    // Add the code to the database
+    const result = await codes.addCode(
+      codeValue,
+      visitorsName,
+      codeStatus,
+      timeExpired
+    );
+    var codex = await codes.generateCode();
+    console.log;
+    if (result) {
+      res.render("residentPages/codeGenerator", {
+        successMessage: "Code generated successfully.",
+      });
+    } else {
+      res.render("residentPages/codeGenerator", {
+        errorMessage: "Failed to generate code. Please try again.",
+      });
+    }
   } catch (err) {
-      console.error(`Error while generating code: `, err.message);
-      res.render("residentPages/codeGenerator", { errorMessage: 'An error occurred while generating the code. Please try again later.' });
+    console.error(`Error while generating code: `, err.message);
+    res.render("residentPages/codeGenerator", {
+      errorMessage:
+        "An error occurred while generating the code. Please try again later.",
+    });
   }
 });
 
